c981519da8f106e54b934e70add6169642d5912d
Brendan <brendankalis2@gmail.com>
2022-02-09 04:07:58 -0600

    commit tree fa36abe5bcae0a02d44553de72c32c555bffda41

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+]
+
+function superbowlWin(record){
+    let win = record.find(array => array.result === "W")
+    if(win)
+    return win.year
+}
